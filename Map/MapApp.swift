cbe006e1d2eef0446b58c71f11073f3cb9cea324
ajs7777 <abhisaha955@gmail.com>
2024-12-20 21:42:43 +0530

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Abhijit Saha on 20/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
